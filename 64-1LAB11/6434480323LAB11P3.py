e0f843fbfc77211085a7bbc276dad0ec1c372e59
ratchanonp <qq.panmas@gmail.com>
2021-11-17 11:00:29 +0700

    :pencil: Change variable name

--- 64-1LAB11/6434480323LAB11P3.py
+++ 64-1LAB11/6434480323LAB11P3.py
@@ -9,12 +9,12 @@ with open(score_file, 'r') as f:
         sum_score = sum(map(int, values[1:]))
         competitors[competitor] = sum_score
 
-    highest3 = []
+    highest = []
     for competitor, score in competitors.items():
-        highest3.append((competitor, score))
+        highest.append((competitor, score))
 
-    highest3.sort(key=lambda x: x[1], reverse=True)
+    highest.sort(key=lambda x: x[1], reverse=True)
 
     print("The winners are :")
     for i in range(3):
-        print(f"{i} : {highest3[i][0]} {highest3[i][1]}")
+        print(f"{i} : {highest[i][0]} {highest[i][1]}")
