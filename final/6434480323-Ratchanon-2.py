fa8d7db336776522f4f73e878dbd4f758a26920f
ratchanonp <qq.panmas@gmail.com>
2021-11-24 09:01:52 +0700

    :art: Prettier

--- final/6434480323-Ratchanon-2.py
+++ final/6434480323-Ratchanon-2.py
@@ -5,13 +5,13 @@ Section : 2
 """
 
 provinces = map(str, input("Provinces: ").split())
+
 patients = []
 for province in provinces:
     patient = int(input(f"patients in {province}: "))
     patients.append((province, patient))
 
 patients.sort(key=lambda x: x[1], reverse=True)
-
 
 print("\nNumber of patients:")
 for province, patient in patients:
